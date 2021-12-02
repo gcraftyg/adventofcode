fe09652c6905ea86ec05fd44fbcf96212175e04a
Greg Craft <1685986+gcraftyg@users.noreply.github.com>
2021-12-02 10:48:20 -0600

    2021 - day 2, part 2

---
+++ 2021/02/02/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const inputPath = process.argv[2];
+
+const input = fs.readFileSync(inputPath, "utf8");
+const inputArr = input.split("\n");
+const movements = inputArr.map(v => v.split(' ')).map(([dir, amount]) => [dir, parseInt(amount)]);
+
+function depth(moves) {
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+
+  for(let [dir, amount] of moves) {
+    if(dir === 'down') {
+      aim += amount;
+    } else if(dir === 'up') {
+      aim -= amount;
+    } else if(dir === 'forward') {
+      horizontal += amount;
+      depth += amount * aim;
+    }
+  }
+
+  return horizontal * depth;
+}
+
+console.log(depth(movements));
+
